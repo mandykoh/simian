fd4d38ef2fcd27772f5847c2f893ac8273e4fef1
Amanda Koh <amanda@naucera.net>
2017-12-20 19:46:29 +1100

    Implement recursive square traversal.

--- math_test.go
+++ math_test.go
@@ -4,6 +4,37 @@ import "testing"
 
 func TestMath(t *testing.T) {
 
+	t.Run("flattenRecursiveSquares()", func(t *testing.T) {
+
+		t.Run("produces a recursive square traversal of a square 2D matrix", func(t *testing.T) {
+			m := []int16{
+				0, 1, 4,
+				2, 3, 6,
+				5, 7, 8,
+			}
+
+			result := flattenRecursiveSquares(m)
+
+			if expected, actual := len(m), len(result); expected != actual {
+				t.Fatalf("Expected result to be of length %d but got %d", expected, actual)
+			}
+
+			for i := 0; i < len(result); i++ {
+				if result[i] != int16(i) {
+					t.Errorf("Expected element %d but got %d", i, result[i])
+				}
+			}
+		})
+
+		t.Run("produces an empty result for empty input", func(t *testing.T) {
+			result := flattenRecursiveSquares([]int16{})
+
+			if actual := len(result); actual != 0 {
+				t.Fatalf("Expected zero length result but got %d", actual)
+			}
+		})
+	})
+
 	t.Run("flattenZigZag()", func(t *testing.T) {
 
 		t.Run("produces a zig-zag rearrangement of a 2D matrix", func(t *testing.T) {
